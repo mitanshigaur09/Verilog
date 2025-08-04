228670af57a4f773e3b7d858a93cb357bbaf87d0
Mitanshi Gaur <118531410+mitanshigaur09@users.noreply.github.com>
2025-08-04 21:26:47 +0530

    Create or_gate_using_nand_tb.v

---
+++ OR Gate using NAND/or_gate_using_nand_tb.v
@@ -0,0 +1,18 @@
+// Code your testbench here
+// or browse Examples
+module or_gate_tb;
+reg a,b;
+wire y;
+  or_gate dut(.a(a),.b(b),.y(y));
+  initial begin
+    $monitor("At time T =%0t,a=%b,b=%b,y=%b",$time,a,b,y);
+a=0;b=0;
+#2 a=0;b=1;
+#2  a=1;b=0;
+#2  a=1;b=1;
+  end
+initial begin
+  $dumpfile("$dump.vcd");
+  $dumpvars(0,a,b,y);
+end
+endmodule
