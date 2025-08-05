7f6ab5728e2f8509d193c5cfbe3b47b18430b5a2
Mitanshi Gaur <118531410+mitanshigaur09@users.noreply.github.com>
2025-08-05 21:44:58 +0530

    Create and_gate_using_nand_tb.v

---
+++ AND Gate using NAND/and_gate_using_nand_tb.v
@@ -0,0 +1,18 @@
+// Code your testbench here
+// or browse Examples
+module andgate_tb;
+reg a,b;
+wire y;
+  andgate dut(.a(a),.b(b),.y(y));
+  initial begin
+    $monitor("At time T=%0t,a=%b,b=%b,y=%b",$time,a,b,y);
+a=0;b=0;
+#2 a=0;b=1;
+#2  a=1;b=0;
+#2  a=1;b=1;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,a,b,y);
+  end
+endmodule
